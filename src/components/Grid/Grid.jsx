76c5730538f433148125d46aecccc16da897fdbc
Grant <gmontgomery1994@gmail.com>
2019-12-11 16:10:45 -0800

    Drag and drop basically working. Going to edit some.

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,27 +1,139 @@
 import React, { Component } from "react";
+import { Piece } from "../Piece";
 import "./Grid.css";
 
 class Grid extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  drop = event => {
+    event.preventDefault();
+    const piece_id = event.dataTransfer.getData("piece_id");
+    const card = document.getElementById(piece_id);
+    event.target.appendChild(card);
+    card.style.display = "block";
+    event.target.appendChild(card);
+  };
+  dragOver = event => {
+    event.preventDefault();
+  };
   render() {
     return (
       <div className="grid-wrapper">
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
-        <div className="grid-square"></div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          <Piece></Piece>
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
+        <div
+          className="grid-square"
+          onDrop={this.drop}
+          onDragOver={this.dragOver}
+        >
+          {this.props.children}
+        </div>
       </div>
     );
   }
---
+++ src/components/Piece/Piece.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./Piece.css";
+
+class Piece extends Component {
+  state = {};
+
+  dragStart = event => {
+    const { target } = event;
+    event.dataTransfer.setData("piece_id", target.id);
+
+    setTimeout(() => {
+      target.style.display = "none";
+    }, 0);
+  };
+
+  dragOver = event => {};
+
+  render() {
+    return (
+      <div
+        draggable="true"
+        id="1"
+        onDragStart={this.dragStart}
+        onDragOver={this.dragOver}
+        className="piece-wrapper"
+      ></div>
+    );
+  }
+}
+
+export default Piece;
